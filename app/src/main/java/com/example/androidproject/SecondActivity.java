fb445645fadda6c4f5f0dc6785ff5ae4d8ca7e7d
LucasGOE <1963125740@qq.com>
2021-04-24 10:04:26 +0800

    20210424

--- app/src/main/java/com/example/androidproject/SecondActivity.java
+++ app/src/main/java/com/example/androidproject/SecondActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SecondActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class SecondActivity extends AppCompatActivity {
+    private BottomNavigationView bnv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        bnv=(BottomNavigationView) findViewById(R.id.bottomNavigationView);
     }
 }
